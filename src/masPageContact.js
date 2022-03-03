fcf2ae815d39e710f2f7957d58f5e7473d9be30d
Andrew Hamilton <16577621+undefined@users.noreply.github.com>
2022-03-03 21:19:04 +0000

    Changes to deposit page, work related to variable modal

--- src/masPageContact.js
+++ src/masPageContact.js
@@ -2,11 +2,10 @@ import { LightningElement } from 'lwc';
 import fetchDataHelper from './fetchDataHelper';
 
 const columns = [
-    { label: 'Label', fieldName: 'name' },
-    { label: 'Website', fieldName: 'website', type: 'url' },
-    { label: 'Phone', fieldName: 'phone', type: 'phone' },
-    { label: 'Balance', fieldName: 'amount', type: 'currency' },
-    { label: 'CloseAt', fieldName: 'closeAt', type: 'date' },
+    { label: 'Name', fieldName: 'name' },
+    { label: 'Company', fieldName: 'company', },
+    { label: 'Role', fieldName: 'phone', type: 'phone' },
+    { label: 'Status', fieldName: 'amount', type: 'currency' }
 ]; 
 
 export default class BasicDatatable extends LightningElement {
