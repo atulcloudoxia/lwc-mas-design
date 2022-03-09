da11c51134696f5ec3046e329ebdbe42eba2291a
Andrew Hamilton <16577621+undefined@users.noreply.github.com>
2022-03-09 11:10:58 +0000

    Populated data in contact modal with selected row

--- src/masTableContact.js
+++ src/masTableContact.js
@@ -29,12 +29,21 @@ const columns = [
 export default class TableContacts extends LightningElement {
 
     @track editContact = false;
+    @track selectedRow = {};
+    @track isCorporation = false;
 
     columns = columns;
 
-    data = [
+    @track data = [
         { 
+            id: 1,
             name: 'Andrew Hamilton',
+            firstname: 'Andrew',
+            lastname: 'Hamilton',
+            language: 'en',
+            dob: '1978-10-29',
+            salutation: 'mr',
+            sin: '123123123',
             role: 'Buyer 1',
             email: 'andrew@lucidlive.com',
             phone: '514-555-5555',
@@ -42,7 +51,14 @@ export default class TableContacts extends LightningElement {
             isComplete: true,
         },
         { 
+            id: 2,
             name: 'Tao-Nhan Nguyen',
+            firstname: 'Tao-Nhan',
+            lastname: 'Nguyen',
+            language: 'fr',
+            dob: '1942-10-09',
+            salutation: 'mr',
+            sin: '123123123',
             role: 'Buyer 2',
             email: 'tao@onyxtech.ca',
             phone: '514-555-5555',
@@ -50,8 +66,15 @@ export default class TableContacts extends LightningElement {
             isComplete: false,
         },
         { 
+            id: 3,
             name: 'Chuck Norris',
-            role: 'Buyer 3',
+            firstname: 'Chuck',
+            lastname: 'Norris',
+            language: 'ch',
+            dob: '1852-10-09',
+            salutation: 'mr',
+            sin: '123123123',
+            role: 'corporation',
             email: 'chuck@norris.ca',
             phone: '514-555-5555',
             status: 'Complete',
@@ -59,6 +82,43 @@ export default class TableContacts extends LightningElement {
         }
     ];
 
+    roleOptions = [
+        { label: 'Buyer 1', value: 'buyer-1'},
+        { label: 'Buyer 2', value: 'buyer-2'},
+        { label: 'Buyer 3', value: 'buyer-3'},
+        { label: 'Buyer 4', value: 'buyer-4'},
+        { label: 'Realtor', value: 'realtor'},
+        { label: '3rd Party', value: '3rd-party'},
+        { label: 'Former Buyer 1', value: 'former-buyer-1'},
+        { label: 'Former Buyer 2', value: 'former-buyer-2'},
+        { label: 'Former Buyer 3', value: 'former-buyer-3'},
+        { label: 'Former Buyer 4', value: 'former-buyer-4'},
+        { label: 'Witness', value: 'witness'},
+        { label: 'Corporation', value: 'corporation'},
+        { label: 'Referred by Client', value: 'referred-by-client'},
+    ];
+
+    salutationOptions = [
+        { label: 'Mr.', value: 'mr'},
+        { label: 'Ms.', value: 'ms'},
+        { label: 'Mrs.', value: 'mrs'},
+        { label: 'Dr.', value: 'dr'},
+        { label: 'Prof.', value: 'prof'},
+    ];
+
+    languageOptions = [
+        { label: 'English', value: 'en'},
+        { label: 'French', value: 'fr'},
+        { label: 'Chinese.', value: 'ch'},
+    ];
+
+    handleRoleChange(event) {
+        let role = event.detail.value;
+
+        this.isCorporation = role === 'corporation';
+        this.selectedRow.role = role;
+    }
+   
     /**
      * Row actions
      */
@@ -83,8 +143,11 @@ export default class TableContacts extends LightningElement {
      */
     editRow(row) {
         const { id } = row;
-        const index = this.findRowById(id);
+        //const index = this.findRowById(id);
         
+        this.selectedRow = row;
+        //this.selectedRow = Object.assign(row, {});
+        this.isCorporation = row.role === 'corporation';
         this.editContact=true;
     }
 
