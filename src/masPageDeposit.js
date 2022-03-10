7053d86aa0d8cbf2e44d43f14778565e7619a2e7
andrew hamilton <andrewhamilton@andrews-MacBook-Pro.local>
2022-03-10 16:13:00 -0500

    Cleaned up the data on the deposit screen

--- src/masPageDeposit.js
+++ src/masPageDeposit.js
@@ -1,6 +1,81 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class Deposit extends LightningElement {
+const rowActions = [
+  {
+    label: 'Edit',
+    name: 'edit'
+  },
+  {
+    label: 'Delete',
+    name: 'delete'
+  }
+];
 
+const columns = [
+  {
+    label: 'Payment Number',
+    fieldName: 'number',
+    type: 'number',
+    editable: true,
+    hideDefaultActions:true,
+  },
+  {
+    label: 'Date',
+    fieldName: 'date',
+    type: 'date',
+    editable: true,
+    hideDefaultActions:true,
+  },
+  {
+    label: 'Deposit To',
+    fieldName: 'deposit_to',
+    type:'select',
+    editable: true
+  },
+  {
+    label: 'Deposit For',
+    fieldName: 'deposit_for',
+    editable: true
+  },
+  {
+    label: 'Amount',
+    fieldName: 'amount',
+    editable: true
+  },
+  {
+    label: 'Percent',
+    fieldName: 'percent',
+    type: 'percent',
+    editable: true
+  },
+  {
+    label: 'Received',
+    fieldName: 'received',
+    type: 'boolean',
+    editable: true
+  },
+  {
+    type: 'action',
+    typeAttributes: {
+      rowActions
+    },
+  },
+];
+
+
+export default class DepositTable extends LightningElement {
+    columns = columns;
+
+    @track data = [
+      {
+          id: 1,
+          number: 1,
+          date: '09/01/2022',
+          deposit_to: 'notary',
+          deposit_for: 'cashdown',
+          amount: "100000",
+          percent: "0.01",
+          received: false
+      },
+    ];
 }
-
--- src/masTableExtrasAdd.js
+++ src/masTableExtrasAdd.js
@@ -90,4 +90,47 @@ export default class BasicDatatable extends LightningElement {
           price: '87272'
       },
     ];
+
+    /**
+     * Row actions
+     */
+    handleRowAction(event) {
+      const { action, row } = event.detail;
+
+      switch (action.name) {
+        case 'delete':
+          this.deleteRow(row);
+          break;
+        // No other actions but delete for now
+        default:
+      }
+    }
+
+    /**
+     * Delete
+     */
+    deleteRow(row) {
+      const { id } = row;
+      const index = this.findRowById(id);
+
+      if (index !== -1) {
+        this.data = this.data
+          .slice(0, index)
+          .concat(this.data.slice(index + 1));
+
+        // Nhan, handle delete logic here
+      }
+    }
+
+    findRowById(id) {
+      let ret = -1;
+      this.data.some((row, index) => {
+        if (row.id === id) {
+          ret = index;
+          return true;
+        }
+        return false;
+      });
+      return ret;
+    }
 }
