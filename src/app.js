d5a381b1d8078393652ec11bf912f3fa796a0344
andrew hamilton <andrewhamilton@andrews-MacBook-Pro.local>
2022-03-15 15:31:09 -0400

    Created notification icons for page states

--- src/app.js
+++ src/app.js
@@ -2,99 +2,61 @@ import { LightningElement, api, track } from 'lwc';
 
 export default class Mas extends LightningElement {
 
-  @track navigation = {
-    pageIndex:0,
-    pages: [
-      {
-        id: "contact",
-        step: 1,
-        label: "Contact",
-        selected:true
-      },
-      {
-        id: "asset",
-        step: 2,
-        label: "Asset",
-        selected:false
-      },
-      {
-        id: "deposit",
-        step: 3,
-        label: "Deposit",
-        selected:false
-      },
-      {
-        id: "varia",
-        step: 4,
-        label: "Varia",
-        selected:false
-      },
-      {
-        id: "files",
-        step: 5,
-        label: "Files",
-        selected:false
-      },
-      {
-        id: "review",
-        step: 6,
-        label: "Review",
-        selected:false
-      }
-    ],
-    get title() {
-      return this.pages[this.pageIndex].label;
-    },
-    get currentPage() {
-      return this.pages[this.pageIndex].id;
-    },
-    set currentPage(index) {
-      this.pageIndex=index;
-      this.pages[index].selected=true;
-      this.pages.forEach((item, idx) => this.pages[idx].selected = idx === parseInt(index))
-    },
-    get showPages() {
-      return this.pages;
-    }
-  }
-
   @track
   pages = [
     {
       id: "contact",
       step: 1,
-      label: "Contact22",
-      selected:true
+      label: "Contact",
+      selected:true,        // Current page in view
+      completed:true,       // Page is completed
+      warning: false,       // Page has warning
+      error: false,         // Page has error
     },
     {
       id: "asset",
       step: 2,
       label: "Asset",
-      selected:false
+      selected:false,
+      completed:false,
+      warning: true,
+      error: false,
     },
     {
       id: "deposit",
       step: 3,
       label: "Deposit",
-      selected:false
+      selected:false,
+      completed:false,
+      warning: false,
+      error: false,
     },
     {
       id: "varia",
       step: 4,
       label: "Varia",
-      selected:false
+      selected:false,
+      completed:false,
+      warning: false,
+      error: false,
     },
     {
       id: "files",
       step: 5,
       label: "Files",
-      selected:false
+      selected:false,
+      completed:false,
+      warning: false,
+      error: true,
     },
     {
       id: "review",
       step: 6,
       label: "Review",
-      selected:false
+      selected:false,
+      completed:false,
+      warning: false,
+      error: false,
     }
   ];
 
